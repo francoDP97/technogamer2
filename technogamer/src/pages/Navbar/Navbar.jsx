79e0eb38d4d9d6f34bf0af99d9bce471b8c1e46c
FrancoGerez <franconicolasgerez1998@gmail.com>
2025-04-08 23:29:12 -0300

    inicio_de_navabar2

--- technogamer/src/pages/Navbar/Navbar.jsx
+++ technogamer/src/pages/Navbar/Navbar.jsx
@@ -2,21 +2,22 @@ import React from "react";
 import isoTipo from '../../assets/img/isotipo_technogamer.svg';
 import buscador from '../../assets/img/iconos/buscador.png';
 import carrito from '../../assets/img/iconos/carrito.png';
+import './Navbar.css';
 function Navbar() {
     return (
         <nav>
-            <div>
+            <div className="logo">
                 <img src={isoTipo} alt="Logo Technogamer" />
                 <p>TECHNOGAMER</p>
             </div>
-            <div>
-                <ul>
+            <div className="enlaces">
+                <ul >
                     <li>Productos</li>
                     <li>Soporte</li>
                     <li>Nosotros</li>
                 </ul>
             </div>
-            <div>
+            <div className="icons">
                 <img src={buscador} alt="Logo Technogamer" />
                 <img src={carrito} alt="Logo Technogamer" />
             </div>
