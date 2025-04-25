e470dfd95be02cd39d00acc97299cb9c9216245e
FrancoGerez <franconicolasgerez1998@gmail.com>
2025-04-24 21:46:39 -0300

    correcion de los card

--- technogamer/src/pages/CatalogoProducto/Item/Item.jsx
+++ technogamer/src/pages/CatalogoProducto/Item/Item.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import Button from "react-bootstrap/Button";
-function Item({producto}) {
-  return (     
-    <div className="col-sm-6 col-md-4 col-lg-3 mb-4">      
-        <div className="card h-100">
-          <img
-            src={producto.image}
-            className="card-img-top"
-            alt={producto.image}            
-          />
-          <div className="card-body d-flex flex-column justify-content-between">
-            <div>
-              <h5 className="card-title">{producto.name}</h5>
-              <p className="card-text fw-bold">${producto.price}</p>
-            </div>
-            <div className="d-flex flex-column  gap-2 mt-3">
-              <Button className="btn-sm" as={Link} to={`/producto/${producto.id}`} variant="primary">
-                Ver Detalle
-              </Button>
-              <Button className="btn-sm" variant="success">
-                Agregar al carrito
-              </Button>
-            </div>
+function Item({ producto }) {
+  return (
+    <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
+      <div className="card h-100">
+        <img
+          src={producto.image}
+          className="card-img-top"
+          alt={producto.image}
+        />
+        <div className="card-body d-flex flex-column justify-content-between">
+          <div>
+            <h5 className="card-title">
+              {producto.name.length > 50
+                ? producto.name.slice(0, 50) + '...'
+                : producto.name}
+            </h5>
+            <p className="card-text fw-bold">${producto.price}</p>
+          </div>
+          <div className="d-flex flex-column  gap-2 mt-3">
+            <Button className="btn-sm" as={Link} to={`/producto/${producto.id}`} variant="primary">
+              Ver Detalle
+            </Button>
+            <Button className="btn-sm" variant="success">
+              Agregar al carrito
+            </Button>
           </div>
         </div>
+      </div>
     </div>
   )
 }
--- technogamer/src/pages/CatalogoProducto/data/productos.js
+++ technogamer/src/pages/CatalogoProducto/data/productos.js
@@ -2,7 +2,7 @@ const productos = [
   {
     id: 1,
     name: "Teclado Mecanico Logitech PRO X TKL Wireless Lightspeed 1ms White RGB Switch GX 50Hs",
-    price: 29900,
+    price: 29400.00,
     image: "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_39742_Teclado_Mecanico_Logitech_PRO_X_TKL_Wireless_Lightspeed_1ms_White_RGB_Switch_GX_50Hs_e564c8a6-med.jpg",
     category: "Teclado",
     subcategory: "Logitech", // nuevo
@@ -12,7 +12,7 @@ const productos = [
   {
     id: 2,
     name: "Teclado Mecanico Logitech PRO X 60 Wireless Lightspeed 2.4Ghz Bluetooth Black Switch GX RGB 65Hs",
-    price: 29900,
+    price: 12900.00,
     image: "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_39638_Teclado_Mecanico_Logitech_PRO_X_60_Wireless_Lightspeed_2.4Ghz_Bluetooth_Black_Switch_GX_RGB_65Hs_c5716463-med.jpg",
     category: "Teclado",
     subcategory: "Logitech", // nuevo
@@ -22,7 +22,7 @@ const productos = [
   {
     id: 3,
     name: "Teclado Mecanico Logitech G915X Wireless tri-mode Low Profile Lightspeed TKL Switch Tactil Black",
-    price: 29900,
+    price: 20900.00,
     image: "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_42700_Teclado_Mecanico_Logitech_G915X_Wireless_tri-mode_Low_Profile_Lightspeed_TKL_Switch_Tactil_Black_4a5f50dd-grn.jpg",
     category: "Teclado",
     subcategory: "Logitech", // nuevo
@@ -32,37 +32,40 @@ const productos = [
   {
     id: 4,
     name: "Teclado Mecanico Redragon K686 Eisa PRO Wireless 2.4GHz Bluetooth RGB Switch Red Linear White/Blue",
-    price: 79.99,
+    price: 7999.00,
     image: "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43138_Teclado_Mecanico_Redragon_K686_Eisa_PRO_Wireless_2.4GHz_Bluetooth_RGB_Switch_Red_Linear_White_Blue_6c900b83-grn.jpg",
     category: "Teclado",
     subcategory: "Redragon",
     description: "Webcam Full HD 1080p con micrófono estéreo.",
-    stock: 10
+    stock: 11
   },
   {
-    id: 4,
-    name: "Auriculares Razer Kraken X",
-    price: 49.99,
-    image: "https://m.media-amazon.com/images/I/61Qe0euJJZL._AC_SL1500_.jpg",
-    category: "Periféricos",
+    id: 5,
+    name: "Teclado Mecanico Redragon K686 Eisa PRO SE Wireless 2.4Ghz Bluetooth RGB Switch Red Linear",
+    price: 87400.00,
+    image: "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43022_Teclado_Mecanico_Redragon_K686_Eisa_PRO_SE_Wireless_2.4Ghz_Bluetooth_RGB_Switch_Red_Linear__268ac322-grn.jpg",
+    category: "Teclado",
+    subcategory: "Redragon",
     description: "Sonido envolvente 7.1, diseño ligero para largas sesiones.",
     stock: 10
   },
   {
-    id: 5,
-    name: "Micrófono Blue Yeti",
-    price: 129.99,
-    image: "https://m.media-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg",
-    category: "Accesorios",
+    id: 6,
+    name: "Teclado Mecanico Redragon K648 Olaf Swtich Red Lineal RGB USB-C 90%",
+    price: 83150.00,
+    image: "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_43025_Teclado_Mecanico_Redragon_K648_Olaf_Swtich_Red_Lineal_RGB_USB-C_90__79eea7b6-grn.jpg",
+    category: "Teclado",
+    subcategory: "Redragon",
     description: "Micrófono USB multipatrón para streaming y grabación.",
     stock: 10
   },
   {
     id: 6,
-    name: "AMD Ryzen 5 5600X",
-    price: 229.99,
-    image: "https://m.media-amazon.com/images/I/71WPGXQLcLL._AC_SL1500_.jpg",
-    category: "Hardware",
+    name: "Teclado Hypermagnetico SteelSeries Apex PRO TKL Wireless 2.4Ghz Bluetooth OLED Smart Display Switch Omnipoint 3.0 Hall Effect RGB 38hs",
+    price: 300600.00,
+    image: "https://imagenes.compragamer.com/productos/compragamer_Imganen_general_45706_Teclado_Hypermagnetico_SteelSeries_Apex_PRO_TKL_Wireless_2.4Ghz_Bluetooth_OLED_Smart_Display_Switch_Omnipoint_3.0_Hall_Effect_RGB_38hs_686cbc35-grn.jpg",
+    category: "Teclado",
+    subcategory: "SteelSeries",
     description: "6 núcleos, 12 hilos, excelente rendimiento en juegos.",
     stock: 10
   },
